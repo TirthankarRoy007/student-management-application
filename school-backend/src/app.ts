878d22ab2b1639a884b6db17d3ba8978d6558f09
Tirthankar Roy <tirthankar@wemotiveforge.com>
2026-01-22 21:44:00 +0530

    fix: fixed dot env

--- school-backend/src/app.ts
+++ school-backend/src/app.ts
@@ -1,4 +1,5 @@
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 import express, { type Application, type Request, type Response, type NextFunction } from "express";
 import cors from "cors";
 import helmet from "helmet";
