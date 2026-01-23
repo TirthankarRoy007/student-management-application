853e062cd79ab1e5b14d4695b70147b81b96d649
Tirthankar Roy <tirthankar@wemotiveforge.com>
2026-01-23 15:02:00 +0530

    fix: fixed url

--- school-frontend/src/api/axios.ts
+++ school-frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { storage } from "../utils/storage";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1", // Fallback if env not set
+  baseURL: import.meta.env.VITE_API_URL || "https://student-management-application-y4fd.onrender.com/api/v1", // Fallback if env not set
   headers: {
     "Content-Type": "application/json",
   },
